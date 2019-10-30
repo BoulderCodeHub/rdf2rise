c2d4c06547420fdf03f91cac3cae48211694a52e
Alan Butler <rabutler@usbr.gov>
2019-10-30 14:57:26 -0600

    initial tests for `rwtbl_add_rise_vars()`
    includes sample rdf file

--- tests/testthat/test-rwtbl_add_rise_vars.R
+++ tests/testthat/test-rwtbl_add_rise_vars.R
@@ -1,3 +1,62 @@
-test_that("multiplication works", {
-  expect_equal(2 * 2, 4)
+library(RWDataPlyr)
+
+# initial setup -----------------------
+ifile <- "../KeySlots.rdf"
+keep_cols <- c("Timestep", "TraceNumber","ObjectName", "SlotName", "Value",
+  "Unit", "RulesetFileName", "InputDMIName")
+
+bad_tbl <- rdf_to_rwtbl2(ifile, scenario = "DNF,CT,IG")
+bad_tbl2 <- rdf_to_rwtbl2(ifile, keep_cols = keep_cols)
+good_tbl <- rdf_to_rwtbl2(ifile, keep_cols = keep_cols, scenario = "DNF,CT,IG")
+
+bad_ui <- list(
+  sourceCode = "CRSS - Test Data",
+  modelNameSourceCode = "CRSS",
+  status = "testing"
+)
+bad_ui2 <- list(
+  sourceCode = "CRSS - Test Data",
+  modelNameSourceCode = "CRSS",
+  status = c("testing", "and more testing"),
+  modelRunDescription = "test description"
+)
+good_ui <- list(
+  sourceCode = "CRSS - Test Data",
+  modelNameSourceCode = "CRSS",
+  status = "testing",
+  modelRunDescription = "test description"
+)
+
+# check errors ----------------------
+test_that("rwtbl_add_rise_vars() errors correctly", {
+  expect_error(rwtbl_add_rise_vars(bad_tbl, good_ui))
+  expect_error(rwtbl_add_rise_vars(bad_tbl2, good_ui))
+  expect_error(rwtbl_add_rise_vars(good_tbl, bad_ui))
+  expect_error(rwtbl_add_rise_vars(good_tbl, bad_ui2))
+})
+
+# check return type/value ----------------
+expt_cols <- c(
+  "sourceCode",
+  "locationSourceCode",
+  "dateTime",
+  "result",
+  "status",
+  "lastUpdate",
+  "resultAttributes",
+  "parameterSourceCode",
+  "modelRunSourceCode",
+  "modelRunName",
+  "modelRunDescription",
+  "modelRunAttributes",
+  "modelRunMemberSourceCode",
+  "modelRunMemberDesc",
+  "modelRunDateTime",
+  "modelNameSourceCode"
+)
+test_that("rwtbl_add_rise_vars() returns as expected", {
+  expect_is(x <- rwtbl_add_rise_vars(good_tbl, good_ui), "tbl_df")
+  expect_equal(nrow(x), nrow(good_tbl))
+  expect_true(all(names(x) %in% expt_cols))
+  expect_true(all(expt_cols %in% names(x)))
 })
